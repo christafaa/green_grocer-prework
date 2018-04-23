51ab84d21b2d1821e1fa931c6ffaf7a733f4f184
Chris Alberti <christopherjalberti@gmail.com>
2018-04-23 02:11:03 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -24,6 +24,9 @@ def apply_coupons(cart, coupons)
         item_stats[:count] = item_stats[:count] % coupon[:num]
         result["#{item_name} W/COUPON"] = {:price => coupon[:cost], :clearance => item_stats[:clearance], :count => discount_count}
         result[item_name] = item_stats
+      else
+        result[item_name] = item_stats
+      end
 
 
 end
