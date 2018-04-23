f78f9706aee2cfd0222756adffabd30ee8087791
Chris Alberti <christopherjalberti@gmail.com>
2018-04-23 02:34:08 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -30,6 +30,7 @@ def apply_coupons(cart, coupons)
         result[coupon_key] = {:price => coupon[:cost], :clearance => result[coupon_name][:clearance], :count => 1}
       end
     end
+  end
 
 
 
